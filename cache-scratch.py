cb99439745fea83c165387434cd5e01f2e68b050
tcrundall <tim.crundall@gmail.com>
2016-01-31 17:54:57 +1100

    Add averaging scripts for cache-scratch and linux-scalability

---
+++ cache-scratch.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import re
+import argparse
+import subprocess
+import os
+import statistics
+
+matcher = re.compile("Time elapsed = ([0-9]+\.[0-9]+) seconds.")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--threads', dest='threads', help='number of threads')
+parser.add_argument('-s', '--size', dest='size', help='size of objects')
+parser.add_argument('-v', '--vers', dest='version', help='malloc implementation')
+parser.add_argument('-i', '--iterations', dest='iterations', help='number of iterations')
+
+args = parser.parse_args()
+
+versionList = ["", \
+  "/home/tcrundall/src/Hoard/src/libhoard.so", \
+  "/home/tcrundall/src/gperftools/build/.libs/libtcmalloc.so", \
+  "/home/tcrundall/src/gperftools-edited/build/.libs/libtcmalloc.so"]
+
+benchmark = "/home/tcrundall/src/Hoard/benchmarks/cache-scratch/cache-scratch"
+
+#print(versionList[int(args.version)])
+#print(args)
+
+env = os.environ.copy()
+env['LD_PRELOAD'] = versionList[int(args.version)]
+
+with open("results/temp.txt", 'w') as tempFile:
+  for i in range(0,int(args.iterations)):
+    subprocess.call([benchmark,  
+                         args.threads, '100',                       
+                         args.size,    '1000000'],
+                         stdout=tempFile, env=env)
+
+results = []
+with open("results/temp.txt", 'r') as tempFile:
+  for line in tempFile:
+    results.append(float(matcher.match(line).group(1)))
+
+#print(results)
+
+average = statistics.mean(results)
+stdev = statistics.stdev(results)
+with open("results/temp.txt", 'w') as tempFile:
+  tempFile.write(str(average) + '\t' + str(stdev) + '\n')
---
+++ cache-scratchStatsGathererObjectSize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import re
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--threads', dest='threads', help='number of threads')
+parser.add_argument('-i', '--iterations', dest='iterations', help='number of iterations')
+
+args = parser.parse_args()
+
+
+for i in [2, 4, 8, 16, 32, 64]:
+  with open("results/temp" + str(i) + ".txt", 'w') as writeFile:
+    for j in range(0, 4):
+      subprocess.call(['./cache-scratch.py', '-t', str(args.threads), '-s', str(i),
+                                             '-v', str(j),            '-i', str(args.iterations)])
+      with open("results/temp.txt", 'r') as tempFile:
+        line = tempFile.readline()
+        print("read line is: " + line)
+        writeFile.write(line)
