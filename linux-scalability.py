25cac7c0ba336b17a43aee375a5abf1b3e14529b
Timothy David Crundall <u5018130@anu.edu.au>
2016-02-26 00:03:29 +1100

    Prep linux-scalability and threadtest

--- linux-scalability.py
+++ linux-scalability.py
@@ -15,13 +15,9 @@ parser.add_argument('-i', '--iterations', dest='iterations', help='number of ite
 
 args = parser.parse_args()
 
-versionList = ["", \
-  "/home/tcrundall/src/Hoard/src/libhoard.so", \
-  "/home/tcrundall/src/gperftools/build/.libs/libtcmalloc.so", \
-  "/home/tcrundall/src/gperftools-edited/build/.libs/libtcmalloc.so", \
-  "/home/tcrundall/src/scalloc/out/libscalloc-x86_64.so"]
+versionList = ["", "HOARD_PATH", "TCMALLOC_PATH", "EDIT_TCMALLOC_PATH", "SCALLOC_PATH"]
 
-benchmark = "/home/tcrundall/src/Hoard/benchmarks/linux-scalability/linux-scalability"
+benchmark = "BENCHMARK_PATH/linux-scalability/linux-scalability"
 
 env = os.environ.copy()
 env['LD_PRELOAD'] = versionList[int(args.version)]
